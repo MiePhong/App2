6cfee93fc2664c78b8dae3768a67d4f2d7f231b0
Thanh Phúc <casioplus58@gmail.com>
2023-07-04 19:03:29 +0700

    add public && config Folder

---
+++ src/config/connectDB.js
@@ -0,0 +1,11 @@
+// const config = {
+//     user: "sa",
+//     password: "1111",
+//     server: "localhost\\THANHPHUC",
+//     database: "projectLTW",
+//     options: {
+//       encrypt: false,
+//     },
+//   };
+
+//   export default config;
